c59b6dcd8cdd067516068525332a359e34289d84
taylorobrien <tobrie10@gmu.edu>
2015-02-10 18:53:20 -0500

    Try2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.onload = function() {
- var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+ var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update} );
    
 function preload() {
 
